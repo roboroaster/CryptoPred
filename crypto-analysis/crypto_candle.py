abb29e5578e2aedc4a2892bb4efa5cbbbdd605e0
Rachit <75543479+roboroaster@users.noreply.github.com>
2022-01-31 20:28:29 +0530

    adding all the initial files

---
+++ crypto-analysis/crypto_candle.py
@@ -0,0 +1,10 @@
+import pandas_datareader as web 
+import datetime as dt 
+import mplfinance as mpf
+
+start  = dt.datetime(2021,1,1)
+end = dt.datetime.now()
+
+data = web.DataReader('BTC-USD','yahoo',start,end)
+
+mpf.plot(data,type='candle',volume=True,style='yahoo')
