ac7e37d17118295bc2c58514d70d31bb08f386c2
Rachit <75543479+roboroaster@users.noreply.github.com>
2022-02-01 15:18:28 +0530

    corrected the errors in the python file

--- cryptopred.py
+++ cryptopred.py
@@ -1,9 +1,13 @@
-# # %%
-# %pip install tensorflow
-# %pip install pandas_datareader
-
 # %% [markdown]
-# ### Importing important Librariese
+# ### Importing important Libraries
+
+# %%
+# %pip install numpy 
+# %pip install pandas 
+# %pip install matplotlib
+# %pip install pandas-datareader
+# %pip install torch
+# %pip install tensorflow
 
 # %%
 import numpy as np 
@@ -11,6 +15,7 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import pandas_datareader as web 
 import datetime as dt
+import pandas as pd
 
 
 from sklearn.preprocessing import MinMaxScaler
@@ -19,13 +24,10 @@ from tensorflow.keras.models import Sequential
 from torch import inverse
 from torch_optimizer import Optimizer
 
-
 # %%
 #  Selecting Cryptocurrency and currency comparater
-crypto_currency = 'BTC'
-against_currency = 'USD'
-
-
+crypto_currency = 'DOGE'
+against_currency = 'INR'
 
 # %%
 #  Setting time frame for the training data 
@@ -37,12 +39,9 @@ end = dt.datetime.now()
 #  Loading Financial Data
 data = web.DataReader(f'{crypto_currency}-{against_currency}','yahoo',start,end)
 
-# print(data.head())
-
 # %%
 #  Preparing Data
 scaler = MinMaxScaler(feature_range=(0,1)) # Scaling data between 0 and 1 to make it easy for nn to work
-
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
 # %%
@@ -61,6 +60,7 @@ for x in range(prediction_days,len(scaled_data)):
 x_train,y_train = np.array(x_train),np.array(y_train)
 x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
 
+# %%
 # Create the neural network 
 # pip install numpy == 1.19.5 
 # use this command if there is a problem with numpy 
@@ -80,6 +80,7 @@ model.compile(optimizer='adam',loss='mean_squared_error')
 
 model.fit(x_train,y_train,epochs=25,batch_size=32)
 
+# %%
 # Testing the model 
 
 test_start = dt.datetime(2020,1,1) # (year.month,day)
@@ -109,12 +110,13 @@ for x in range(prediction_days,len(model_inputs)):
 x_test = np.array(x_test)
 x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
 
-
+# %%
 #  Prediction 
 
 prediction_prices = model.predict(x_test)
 prediction_prices = scaler.inverse_transform(prediction_prices)
 
+# %%
 # plotting prediction v/s actual
 
 plt.plot(actual_prices,color='black',label='Actual Prices')
@@ -125,20 +127,20 @@ plt.ylabel('Price')
 plt.legend(loc='upper left')
 plt.show()
 
-
-
+# %%
 # Predict next day 
 
-real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs)+1,0]]
+# real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs)+1,0]]
+real_data = [model_inputs[len(model_inputs) - prediction_days : len(model_inputs)+1, 0]]
 real_data = np.array(real_data)
 real_data = np.reshape(real_data,(real_data.shape[0],real_data.shape[1],1))
 
 prediction = model.predict(real_data)
 prediction = scaler.inverse_transform(prediction)
 
-print(prediction)
+print('{} Price Tomorrow = {} {}'.format(crypto_currency,prediction[0,0],against_currency))
 
-
-
+# %%
+# actual_prices
 
 
